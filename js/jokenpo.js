fbcfa44ee3363bae84a6fbbd683623b0d1913cba
Jampa Matos <jp.coutm@gmail.com>
2021-10-28 21:03:31 -0300

    Finished the script part!

--- js/jokenpo.js
+++ js/jokenpo.js
@@ -13,8 +13,8 @@ let computerScore = 0;
 let playerScore = 0;
 
 computerPlay();
-playerPlay();
-playRound(computerChoice, playerChoice);
+playGame();
+
 
 
 function computerPlay(){
@@ -34,12 +34,29 @@ function playerPlay(){
 
 function playRound(computerChoice, playerChoice){
     if (computerChoice === 'scissor' && playerChoice === 'paper' || computerChoice === 'paper' && playerChoice === 'rock' || computerChoice === 'rock' && playerChoice === 'scissor'){
-        console.log("Computer won!");
-        computerScore++;
+        ++computerScore;
+        console.log("Computer won! Computer now has " + computerScore);
     } else if (playerChoice === 'scissor' && computerChoice === 'paper' || playerChoice === 'paper' && computerChoice === 'rock' || playerChoice === 'rock' && computerChoice === 'scissor'){
-        console.log("Player won!");
-        playerScore++;
+        ++playerScore;
+        console.log("Player won! Player now has " + playerScore);
     } else {
         console.log("It's a tie!");
     }
+}
+
+function playGame(){
+    let i;
+    for (i = 0; i < 99; i++){
+        computerPlay();
+        playerPlay();
+        playRound(computerChoice, playerChoice);
+        if (playerScore === 3){
+                console.log("You Win! You have " + playerScore + " points, and the computer has " + computerScore + " points.")
+                break;
+        } else if (computerScore === 3){
+            console.log("You Lose! You have " + playerScore + " points, and the computer has " + computerScore + " points.")
+            break;
+        }
+    }
+    
 }
